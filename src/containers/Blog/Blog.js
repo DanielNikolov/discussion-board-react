32e2731711fe0b2b4fa490384cc4328f6a8ad6cf
Daniel Nikolov <dnikolov@gmail.com>
2020-04-09 09:28:12 +0300

    Update

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios';
 import styles from  './Blog.module.css';
 
 const Blog = (props) => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                setPosts(response.data);
+            })
+    })
+
+    const topPosts = posts.map(post => {
+        return <Post key={post.id} title={post.title} />
+    })
     return (
         <div>
             <section className={styles.Posts}>
-                <Post />
-                <Post />
-                <Post />
+                {topPosts}
             </section>
             <section>
                 <FullPost />
